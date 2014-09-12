1528e7488b07faadddaa5ccf8085996590600da6
Nikki Durkin <nikki@99dresses.com>
2014-09-12 17:01:39 +1000

    Solved the challenge

---
+++ UITableViewController Challenge Solution/NDTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NDTableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Nikki Durkin on 9/12/14.
+//  Copyright (c) 2014 Nikki Durkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NDTableViewController : UITableViewController
+
+@end
